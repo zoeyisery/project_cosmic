dc7dd59ff581bafbcfb79d8956c7bb4d2bf20e22
zoé <fkfhjti@gmail.com>
2025-01-20 21:35:04 +0900

    [feat] 유저페이지의 'AuthBanner'의 요소 변경 및 레이아웃 수정
    - AuthBanner: 배너를 버튼 요소에서 div 요소로 변경하여 스타일링 및 이벤트 처리 개선
    - 'userPageContainer': 'display: flex'와 'flex-direction: column'을 추가하여 페이지 콘텐츠가 세로로 배치되도록 레이아웃 수정

--- src/app/user/components/AuthBanner/AuthBanner.tsx
+++ src/app/user/components/AuthBanner/AuthBanner.tsx
@@ -3,9 +3,13 @@ import "@fontsource/material-icons";
 import { Icon } from "@/components/Icon/Icon";
 import styles from "./AuthBanner.module.css";
 
-const AuthBanner = () => {
+interface AuthBannerProps {
+  onClick: () => void;
+}
+
+const AuthBanner: React.FC<AuthBannerProps> = ({ onClick }) => {
   return (
-    <button className={styles.banner}>
+    <div className={styles.banner} onClick={onClick}>
       <div>
         <h4>코스믹 로그인 및 회원 가입</h4>
         <p>무한한 코스믹의 세계를 경험하세요.</p>
@@ -13,7 +17,7 @@ const AuthBanner = () => {
       <div>
         <Icon name="chevron_right"></Icon>
       </div>
-    </button>
+    </div>
   );
 };
 
