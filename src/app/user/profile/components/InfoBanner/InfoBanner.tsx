621c7625fdf6b6c948dd06e1b0880851537cccd9
zoé <fkfhjti@gmail.com>
2025-02-10 00:41:33 +0900

    [feat] 'InfoBanner' 컴포넌트 추가 및 사용자 정보 렌더링
    - UI 구성 및 css 작업
    - /api/users에서 가져온 사용자 데이터를 InfoBanner에서 렌더링
    - Redux 상태에서 사용자 정보를 가져와 닉네임 및 이메일을 표시

---
+++ src/app/user/profile/components/InfoBanner/InfoBanner.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { useEffect, useState } from "react";
+import "@fontsource/material-icons";
+import { Icon } from "@/components/Icon/Icon";
+import styles from "./InfoBanner.module.css";
+
+interface InfoBanner {
+  id: string;
+  nickName?: string;
+  profilePicture?: string;
+  bio?: string;
+}
+
+const InfoBanner: React.FC = () => {
+  const [infoBanners, setInfoBanners] = useState<InfoBanner[]>([]);
+
+  useEffect(() => {
+    const fetchBanners = async () => {
+      try {
+        const response = await fetch("/api/users");
+        const data = await response.json();
+        setInfoBanners(data);
+      } catch (error) {
+        console.error("배너 데이터를 불러오는 중 오류 발생:", error);
+      }
+    };
+    fetchBanners();
+  }, []);
+
+  return (
+    <div className={styles.bannerContianer}>
+      {infoBanners.map((infoBanner) => (
+        <div className={styles.banner}>
+          <div className={styles.bannerPart}>
+            <div className={styles.infoPart}>
+              <img
+                src={infoBanner.profilePicture}
+                className={styles.infoImage}
+              />
+              <div className={styles.infoText}>
+                <h4>{infoBanner.nickName}</h4>
+                <p>{infoBanner.id}</p>
+              </div>
+            </div>
+            <div className={styles.bioPart}>
+              <div className={styles.bioText}>
+                <p>{infoBanner.bio}</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.iconPart}>
+            <Icon name="chevron_right" size="30px"></Icon>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default InfoBanner;
